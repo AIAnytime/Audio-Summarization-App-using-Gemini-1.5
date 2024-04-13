19eafe9e0f28b49b213c027640b88799cca06d4e
AI Anytime <sonu1000raw@gmail.com>
2024-04-14 02:46:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import tempfile
 import os
 import google.generativeai as genai
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Configure Google API for audio summarization
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
